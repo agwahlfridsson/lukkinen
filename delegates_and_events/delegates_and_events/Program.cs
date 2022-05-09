39d40779a31edbc840b9653d7eb78693452cc942
Anders Wahlfridsson <agwahlfridsson@gmail.com>
2022-05-09 16:23:48 +0200

    delegate and event

---
+++ delegates_and_events/delegates_and_events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace delegates_and_events
+{
+    delegate void IntHandler(int value);
+
+    class testclass
+    {
+        internal event IntHandler ValueUpdated;
+        internal void TriggerEvent()
+        { 
+            if(this.ValueUpdated != null)
+                this.ValueUpdated(14);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var obj = new testclass();
+            obj.ValueUpdated += Obj_ValueUpdated; //lyssna på eventet
+            obj.TriggerEvent();                   // kör en metod som triggar eventet
+            obj.ValueUpdated -= Obj_ValueUpdated; //sluta lyssna på eventet
+
+            
+        }
+
+        private static void Obj_ValueUpdated(int value)
+        {
+            // hantera att objektet signalerar att dess värde har ändrats
+        }
+    }
+}
